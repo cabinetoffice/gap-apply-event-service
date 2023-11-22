cd7eae0f1beda149bc3723a71e1fcf93673f7c44
Connor Macqueen <138442814+ConnorTCO@users.noreply.github.com>
2023-11-22 17:00:32 +0000

    Adding a no args constructor to the handler because  AWS Lambda

--- src/main/java/gov/cabinetoffice/eventservice/handler/EventServiceHandler.java
+++ src/main/java/gov/cabinetoffice/eventservice/handler/EventServiceHandler.java
@@ -6,22 +6,38 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.cabinetoffice.ObjectMapperConfig;
+import gov.cabinetoffice.eventservice.config.SecretsManagerConfig;
 import gov.cabinetoffice.eventservice.exceptions.JsonException;
 import gov.cabinetoffice.eventservice.exceptions.MessageProcessingException;
 import gov.cabinetoffice.eventservice.service.EventLogService;
+import gov.cabinetoffice.eventservice.service.SecretsManagerService;
 import gov.cabinetoffice.shared.entity.EventLog;
+import gov.cabinetoffice.shared.repository.EventLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.time.Clock;
 
 @RequiredArgsConstructor
 @Service
 public class EventServiceHandler implements RequestHandler<SQSEvent, Void> {
 
     private final Logger logger = LoggerFactory.getLogger(EventServiceHandler.class);
+    private final SecretsManagerService secretsManagerService;
+    private final EventLogRepository eventLogRepository;
     private final EventLogService eventLogService;
     private final ObjectMapper objectMapper;
+    private final Clock clock = Clock.systemUTC();
+
+    public EventServiceHandler() {
+        objectMapper = new ObjectMapperConfig().getObjectMapper();
+        secretsManagerService = new SecretsManagerService(System.getenv("DB_CREDS_SECRET_ARN"), SecretsManagerConfig.getAwsSecretsManager(), objectMapper);
+        eventLogRepository = new EventLogRepository(System.getProperty("database.schema"), secretsManagerService, clock);
+        eventLogService = new EventLogService(eventLogRepository);
+    }
 
     @Override
     public Void handleRequest(final SQSEvent message, final Context context) {
